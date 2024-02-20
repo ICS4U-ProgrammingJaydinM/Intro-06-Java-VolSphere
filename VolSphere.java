2c463d49830e68cdae58afa87fe339fa1d475061
Jaydin Madore <JaydinMadore@stu.ocsb.ca>
2024-02-20 16:02:25 +0000

    VolSphere

--- VolSphere.java
+++ VolSphere.java
@@ -12,23 +12,29 @@
  public final class VolSphere {
 
    public static void main(String[] args) {
-
-    Scanner userinput = new Scanner(System.in);
+    
+    Scanner input = new Scanner(System.in);
 
     System.out.println(" Enter the radius of a sphere that is positive and is not 0:");
-    float radius = userinput.nextFloat();
-    
+    // Gets the users input
+    float radius = input.nextFloat();
+    // Checks if the user input is less than or = to 0.
     if (radius <= 0) {
+        // If it is less than or to 0 it tells the user and than end the program.
         System.out.println("You can not enter zero or a negative number");
     }
+    //Else it does the math to find the volume of a sphere.
     else {
         final Double firstCal = (4.0f / 3.0f) * Math.PI;
         final Double Volume = firstCal * Math.pow(radius, 3);
-        // Outputs the Volume to Console.
+        //Turns the Volume to 3 decimal places.
         double VolumeRounded = Math.round(Volume * 100) / 100.000;
+        //It tells the user the Volume.
         System.out.println("Your Volume is: " + VolumeRounded + " cm^3 (U^3)");
+
+    input.close();
     }
-    
+
     
   }
 }
